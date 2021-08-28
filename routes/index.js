59b7f95890a595f4159ff0ea5a289245fcdb5b58
cuong pham <cuong.pt0622@outlook.com.vn>
2021-08-28 23:59:55 +0700

    ud

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var user=undefined;
+var express = require('express')
+var router = express.Router()
+var user = undefined
 
-exports.configure=function(params){
-	notes=params;
+exports.configure = function (params) {
+  notes = params
 }
 
-exports.index=router.get('/', function(req, res, next) {
-  res.render('index',{title:'Chat', user:req.user});
-});
-
+exports.index = router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Chat', user: req.user })
+})
